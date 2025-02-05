d9b4e2df216efda3d09c330e405e73017cfba95d
Michal <mpiechulek@gmail.com>
2025-02-05 10:00:47 +0100

    set up the routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,31 @@
 import { Routes } from '@angular/router';
+import { LoginLayoutComponent } from './layout/login-layout/login-layout.component';
+import { SecureLayoutComponent } from './layout/secure-layout/secure-layout.component';
+import { NotFoundComponent } from './layout/not-found/not-found.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: LoginLayoutComponent,
+    pathMatch: 'full',
+  },
+  {
+    path: 'login',
+    component: LoginLayoutComponent,
+  },
+  {
+    path: '',
+    component: SecureLayoutComponent,
+    children: [
+      // {
+      //   path: 'dashboard']
+      //   loadChildren: () => import('./features/dashboard/dashboard.module').then(m => m.DashboardModule)
+      // },
+    ],
+  },
+  {
+    path: '**',
+    component: NotFoundComponent,
+    pathMatch: 'full',
+  },
+];
