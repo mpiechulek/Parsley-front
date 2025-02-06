56a163fa4aa5de8e43437ba00785f16cc4876d93
Michael <47081376+mpiechulek@users.noreply.github.com>
2025-02-06 10:12:31 +0100

    Feature/create login layout (#5)
    * added logn page layeat 1 setp
    
    * adjusted styles

--- src/app/layout/login-layout/login-layout.component.ts
+++ src/app/layout/login-layout/login-layout.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { AuthPageComponent } from "../../features/auth-page/auth-page.component";
 
 @Component({
   selector: 'app-login-layout',
   templateUrl: './login-layout.component.html',
-  styleUrl: './login-layout.component.scss'
+  styleUrl: './login-layout.component.scss',
+  imports: [AuthPageComponent]
 })
 export class LoginLayoutComponent {
 
