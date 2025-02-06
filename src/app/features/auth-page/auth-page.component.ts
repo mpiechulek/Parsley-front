dc2fb535f5826492680a70149a4031fa2f63ec30
Michal <mpiechulek@gmail.com>
2025-02-06 15:06:27 +0100

    refurnished login page

--- src/app/features/auth-page/auth-page.component.ts
+++ src/app/features/auth-page/auth-page.component.ts
@@ -1,4 +1,4 @@
-import { Component, inject } from '@angular/core'
+import { Component, inject } from '@angular/core';
 import {
   FormBuilder,
   FormControl,
@@ -6,23 +6,35 @@ import {
   FormsModule,
   ReactiveFormsModule,
   Validators,
-} from '@angular/forms'
-import { MatCardModule } from '@angular/material/card'
-import { MatFormFieldModule } from '@angular/material/form-field'
-import { MatInputModule } from '@angular/material/input'
-import { MatButtonModule } from '@angular/material/button'
-import { CommonModule } from '@angular/common'
+} from '@angular/forms';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { CommonModule } from '@angular/common';
 
 type ToFormGroup<T> = {
-  [P in keyof T]: FormControl<T[P]>
-}
+  [P in keyof T]: FormControl<T[P]>;
+};
 
 interface LoginForm {
-  email: string | null
-  password: string | null
+  email: string | null;
+  password: string | null;
+}
+interface SignupForm {
+  email: string | null;
+  password: string | null;
+  confirmPassword: string | null;
 }
 
-export type LoginFormControls = ToFormGroup<LoginForm>
+enum AuthFormType {
+  LOGIN = 'login',
+  SIGNUP = 'signup',
+}
+
+export type LoginFormControls = ToFormGroup<LoginForm>;
+export type SignupFormControls = ToFormGroup<SignupForm>;
 
 @Component({
   selector: 'app-auth-page',
@@ -34,46 +46,81 @@ export type LoginFormControls = ToFormGroup<LoginForm>
     MatFormFieldModule,
     MatInputModule,
     MatButtonModule,
+    MatIconModule,
   ],
   templateUrl: './auth-page.component.html',
   styleUrl: './auth-page.component.scss',
 })
 export class AuthPageComponent {
-  formBuilder: FormBuilder = inject(FormBuilder)
-  loginForm: FormGroup<LoginFormControls> = this.initializeForm()
+  formBuilder: FormBuilder = inject(FormBuilder);
+  authForm: FormGroup<LoginFormControls> | FormGroup<SignupFormControls> =
+    this.initializeForm();
+  pwdHide: boolean = false;
+  chosenFormType: 'login' | 'signup' = 'login';
+
   private passwordPattern =
-    '(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[$@$!%*?&])[A-Za-z\d$@$!%*?&].{8,}'
+    '(?=\\D*\\d)(?=[^a-z]*[a-z])(?=.*[#$@$!%*?&])(?=[^A-Z]*[A-Z]).{8,30}';
 
   /**
    *
    * @returns
    */
-  private initializeForm(): FormGroup<LoginFormControls> {
-    return this.formBuilder?.group<LoginFormControls>({
-      email: new FormControl('', [Validators.required, Validators.email]),
-      password: new FormControl('', [
-        Validators.required,
-        // Validators.minLength(8),
-        Validators.pattern(this.passwordPattern),
-      ]),
-    })
+  private initializeForm() {
+    if (this.chosenFormType === 'login') {
+      return this.formBuilder?.group<LoginFormControls>({
+        email: new FormControl('', [Validators.required, Validators.email]),
+        password: new FormControl('', [Validators.required]),
+      });
+    } else {
+      return this.formBuilder?.group<SignupFormControls>({
+        email: new FormControl('', [Validators.required, Validators.email]),
+        password: new FormControl('', [
+          Validators.required,
+          Validators.pattern(this.passwordPattern),
+        ]),
+        confirmPassword: new FormControl('', [
+          Validators.required,
+          Validators.pattern(this.passwordPattern)
+        ]),
+      });
+    }
   }
 
-  get emailFormControl(): FormControl<string> {
-    return this.loginForm.get('email') as FormControl
+  private get loginEmailFormControl(): FormControl<string | null> {
+    return this.authForm.controls.email;
   }
 
-  get passwordFormControl(): FormControl<string> {
-    return this.loginForm.get('password') as FormControl
+  public get loginEmailError(): boolean {
+    return this.loginEmailFormControl.errors?.['required'];
   }
+
+  private get loginPasswordFormControl(): FormControl<string | null> {
+    return this.authForm.controls.password;
+  }
+
+  public get loginPasswordError(): boolean {
+    return this.loginPasswordFormControl.errors?.['required'];
+  }
+
+  // /**
+  //  *
+  //  * @param control
+  //  * @returns
+  //  */
+  // matchPassword(control: FormControl): { [key: string]: boolean } | null {
+  //   if (this.chosenFormType !== 'signup') return null;
+  //   const password = this.authForm.controls.password.value;
+  //   const confirmPassword = control.value;
+  //   return password === confirmPassword ? null : { passwordMismatch: true };
+  // }
 
   /**
    *
    */
-  onSubmit(): void {
-    if (this.loginForm.valid) {
-      const formData = this.loginForm.value
-      console.log(formData)
+  onLogin(): void {
+    if (this.authForm.valid) {
+      const formData = this.authForm.value;
+      console.log(formData);
     }
   }
 }
