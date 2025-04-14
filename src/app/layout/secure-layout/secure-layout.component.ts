264c48cac5d71d64ec3a451aaa80e84261e12c2d
Michal <mpiechulek@gmail.com>
2025-04-14 11:44:33 +0200

    added side nav slider

--- src/app/layout/secure-layout/secure-layout.component.ts
+++ src/app/layout/secure-layout/secure-layout.component.ts
@@ -1,13 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { HeaderComponent } from "@shared/components/header/header.component";
+import { HeaderComponent } from '@shared/components/header/header.component';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSidenav, MatSidenavModule } from '@angular/material/sidenav';
 
 @Component({
   selector: 'app-secure-layout',
-  imports: [RouterOutlet, HeaderComponent],
+  imports: [RouterOutlet, HeaderComponent, MatButtonModule, MatSidenavModule],
   templateUrl: './secure-layout.component.html',
-  styleUrl: './secure-layout.component.scss'
+  styleUrl: './secure-layout.component.scss',
 })
 export class SecureLayoutComponent {
+  showFiller = false;
+  @ViewChild('drawer') drawer: MatSidenav | undefined;
 
+  /**
+   * Showing and hiding the side nav
+   */
+  onToggleSideNav(): void {
+    this.drawer?.toggle();
+  }
 }
