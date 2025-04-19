7bdba374bec8b33d371e9801699a61fa5b626045
Michal <mpiechulek@gmail.com>
2025-04-19 15:17:03 +0200

    added grid layout in dashboard

--- src/app/features/dashboard/gender-picker/gender-picker.component.ts
+++ src/app/features/dashboard/gender-picker/gender-picker.component.ts
@@ -1,41 +1,18 @@
-import { AsyncPipe } from '@angular/common';
-import { Component, OnInit } from '@angular/core';
-import { FormControl, FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { MatAutocompleteModule } from '@angular/material/autocomplete';
-import { MatFormFieldModule } from '@angular/material/form-field';
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { MatInputModule } from '@angular/material/input';
-import { map, Observable, startWith } from 'rxjs';
+import { MatSelectModule } from '@angular/material/select';
+import { MatFormFieldModule } from '@angular/material/form-field';
 
 @Component({
   selector: 'app-gender-picker',
-  imports: [
-    FormsModule,
-    MatFormFieldModule,
-    MatInputModule,
-    MatAutocompleteModule,
-    ReactiveFormsModule,
-    AsyncPipe,
-  ],
+  imports: [MatFormFieldModule, MatSelectModule, MatInputModule, FormsModule],
   templateUrl: './gender-picker.component.html',
   styleUrl: './gender-picker.component.scss',
 })
-export class GenderPickerComponent implements OnInit {
-  myControl = new FormControl('');
-  options: string[] = ['Male', 'Female'];
-  filteredOptions!: Observable<string[]>;
-
-  ngOnInit(): void {
-    this.filteredOptions = this.myControl.valueChanges.pipe(
-      startWith(''),
-      map((value) => this._filter(value || '')),
-    );
-  }
-
-  private _filter(value: string): string[] {
-    const filterValue = value.toLowerCase();
-
-    return this.options.filter((option) =>
-      option.toLowerCase().includes(filterValue),
-    );
-  }
+export class GenderPickerComponent {
+  genders = [
+    { value: 'male', viewValue: 'Male' },
+    { value: 'female', viewValue: 'Female' },
+  ];
 }
